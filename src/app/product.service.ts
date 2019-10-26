b2e9fe3974da4a348834d8da4b25c16183f32e46
Abdulraheem Gasali <38100145+Ghazalcode@users.noreply.github.com>
2019-10-26 13:58:42 +0100

    add product list componet

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,14 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import {Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   //creating variable to house album url
  private _albumUrl: string = '../assets/album.json';
+ private _productsUrl: string = '../assets/products.json';
   constructor(private _http:Http) {
 
   }
@@ -17,5 +19,11 @@ export class ProductService {
   getAlbum(id: number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+  }
+
+
 
 }
